1f76e938f65c36c5545b73a548c5a3bfc62908ae
Zachary Hernandez Valverde <artesculinariasgeovanita@gmail.com>
2021-05-17 11:55:10 -0600

    header-view changes

--- artesculinarias.com/src/pages/home/header/organisms/header-view.js
+++ artesculinarias.com/src/pages/home/header/organisms/header-view.js
@@ -5,7 +5,7 @@ function HeaderView(props) {
     return (
         <div className="HeaderView">
             <h5 className="header__text-title">BIENVENIDOS A</h5>
-            <h1 className="header__text-subtitle">arte culinario geovanita</h1>
+            <h1 className="header__text-subtitle">Arte Culinario Geovanita</h1>
         </div>
     );
 }
